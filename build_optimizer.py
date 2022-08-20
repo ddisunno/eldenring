7aa685b34ece30ff2c4280d50220002e30fdfdde
rsd3123 <57509739+rsd3123@users.noreply.github.com>
2022-08-20 00:43:01 -0400

    spreadsheet api

--- build_optimizer.py
+++ build_optimizer.py
@@ -7,11 +7,23 @@ project goals:
     2. optimal gear
     3. write build to .txt file
 """
-
 import requests
 import pandas as pd
+import pygsheets
+#authorization
+gc = pygsheets.authorize(service_file='eldenring/elden-ring-build-optimizer-c393835be6d1.json')
 
-url = "https://github.com/ddisunno/Elden-Ring-Optimizer.git"
+# Create empty dataframe
+df = pd.DataFrame()
 
-x = requests.get(url)
-print(x.status_code)
+# Create a column
+df['name'] = ['John', 'Steve', 'Sarah']
+
+#open the google spreadsheet (where 'PY to Gsheet Test' is the name of my sheet)
+sh = gc.open('Elden Ring Weapon Calculator 2')
+
+#select the first sheet 
+wks = sh[0]
+
+#update the first sheet with df, starting at cell B2. 
+wks.set_dataframe(df,(1,1))
