ea986c20142e64bf1e34f062990fb963489f052e
ddisunno <97629608+ddisunno@users.noreply.github.com>
2022-08-23 00:11:16 -0400

    need fix pls

--- build_optimizer.py
+++ build_optimizer.py
@@ -4,14 +4,25 @@ Author(s): Dylan DiSunno, Rudy DeSanti
 
 project goals:
     1. min-max stats to user selected weapon
-    2. optimal gear
+    2. optimal gear (armor, talismans)
     3. write build to .txt file
 """
 
 import requests
 import pandas as pd
+import simplejson as json
 
-url = "https://github.com/ddisunno/Elden-Ring-Optimizer.git"
+weapons_url = "https://eldenring.fanapis.com/api/weapons"
 
-requests.get(url)
-print(requests.status_code)
+response = requests.get(weapons_url)
+print(response)
+
+weapon_data = json.loads(response.json)
+data_frame = pd.DataFrame.from_dict(weapon_data)
+print(data_frame)
+
+
+
+
+"""with open('weapons.json', 'w') as json_file:
+    json.dump(response, json_file)"""
