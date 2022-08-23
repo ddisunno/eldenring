d8c78b54167bd82665ea2a629d6ce7dd69c719aa
rsd3123 <57509739+rsd3123@users.noreply.github.com>
2022-08-23 01:26:10 -0400

    hi dylan

---
+++ build_opt.py
@@ -0,0 +1,36 @@
+"""
+Elden Ring Build Optimizer:
+Author(s): Dylan DiSunno, Rudy DeSanti
+project goals:
+    1. min-max stats to user selected weapon
+    2. optimal gear (armor, talismans)
+    3. write build to .txt file
+"""
+
+import requests
+import pandas as pd
+import simplejson as json
+
+def API_pull(classes = True, weapons = False):
+    if classes:
+        write_json('classes', 1)
+
+    if weapons:
+        write_json("weapons",16)
+            
+def write_json(item, pages):
+    data_frame = []
+    
+    for iter in range(pages):
+        url = "https://eldenring.fanapis.com/api/" + item +'?page='+ str(iter)
+        response = requests.get(url).text
+        data = json.loads(response)
+        data_frame.append(data)
+    
+    with open(item + '.json', 'w') as json_file:
+        json.dump(data_frame, json_file)
+
+def main():
+    API_pull()
+
+main()
