47711c2fac75d131ed510292ff528d5021dbc532
ddisunno <97629608+ddisunno@users.noreply.github.com>
2022-08-23 01:39:45 -0400

    haha
    haha

--- build_optimizer.py
+++ build_optimizer.py
@@ -1,41 +1,36 @@
 """
 Elden Ring Build Optimizer:
 Author(s): Dylan DiSunno, Rudy DeSanti
-
 project goals:
     1. min-max stats to user selected weapon
     2. optimal gear (armor, talismans)
     3. write build to .txt file
 """
+
 import requests
 import pandas as pd
-
 import simplejson as json
 
-def API_pull(url_ext):
-    data_frame = []
-    response = requests.get("https://eldenring.fanapis.com/api" + str(url_ext)).text
-    data = json.loads(response)
-    data_frame.append(data)
-    return data_frame
-
-def write_json(classes = True, weapons = False):
-    data_frame = []
-
+def API_pull(classes = True, weapons = False):
     if classes:
-        data_frame = API_pull("classes")
+        write_json('classes', 1)
 
     if weapons:
-        for page in range(16):
-            data_frame += API_pull("weapons?page="+str(page))
+        write_json("weapons",16)
 
-    print(data_frame)
+def write_json(item, pages):
+    data_frame = []
 
-    with open('classes.json', 'w') as json_file:
+    for iter in range(pages):
+        url = "https://eldenring.fanapis.com/api/" + item +'?page='+ str(iter)
+        response = requests.get(url).text
+        data = json.loads(response)
+        data_frame.append(data)
+
+    with open(item + '.json', 'w') as json_file:
         json.dump(data_frame, json_file)
 
 def main():
-    write_json()
+    API_pull()
 
 main()
-
