bdc1add7a4963f485b4e2a2fe1b7117e9ca7672f
ddisunno <97629608+ddisunno@users.noreply.github.com>
2022-09-16 21:43:59 -0400

    get_reqs
    bug driving me mad

--- get_reqs.py
+++ get_reqs.py
@@ -34,11 +34,13 @@ def fetch_from_json(item, file):
 
 def endurance_calc(weight,roll_type):
 	"""Function endurance_calc parses endurance.csv; match equip load to endurance level,
+	account for talismans
 	then returns required endurance to use items at a specified weight with specified roll type
 
 	inputs:
 		weight - weight of an item/character
 		roll_type - ideal roll_type from dictionary ['light']['med']['fat']['']
+		talismans - [list of talismans] if ___ in talismans:
 
 	returns:
 		level of endurance required to have an equip load that can manage inputted weight
@@ -60,10 +62,36 @@ def endurance_calc(weight,roll_type):
 			
 			if weight <= equip_load_req: # if weight is less than equip load requirement
 				return int(level[0]) #column 1: endurance level
+
+
+def vigor_calc(desired_health):
+	"""Function vigor_calc parses vigor.csv; match HP to vigor level,
+	account for talismans
+
+	inputs:
+		desired_health - amount of health you would like to have
+
+	outputs:
+		level of vigor required to have desired health
+
+	"""
+	with open('vigor.csv') as f:
+		vigor_scaling = csv.reader(f)
+
+		next(vigor_scaling) #skip header
+
+		for level in vigor_scaling:
+
+			HP = level[1]
+
+			#print(f"{level[0]}: {HP}")
+
+			if int(HP) >= desired_health:
+				return int(level[0])
 			
 
 
-def get_reqs(item, file, roll_type):
+def get_reqs(item, file, roll_type, desired_health):
 	"""Function get_reqs uses fetch_from_json to find an item
 	and generate a list of required item stats
 
@@ -71,6 +99,7 @@ def get_reqs(item, file, roll_type):
 		item - the item to get stat requirements of
 		file - the file that contains the item
 		roll_type - the desired roll type with said item equipped
+		desired_health - the desired health of build
 
 	outputs:
 		required stats [vigor,mind,endurance,strength,dexterity,intelligence,faith,arcane]
@@ -83,7 +112,7 @@ def get_reqs(item, file, roll_type):
 	weight = item_info.get('weight')
 
 	# UNIVERSAL REQUIREMENTS
-	vigor_req 	  = 1
+	vigor_req 	  = vigor_calc(desired_health)
 	mind_req 	  = 1
 	endurance_req = endurance_calc(weight,roll_type)
 
@@ -127,15 +156,107 @@ def get_reqs(item, file, roll_type):
 	elif file == 'armors.json':
 		pass
 
-	return [vigor_req, mind_req, endurance_req, 
+	req_stats = [vigor_req, mind_req, endurance_req, 
 	strength_req, dexterity_req, intelligence_req, faith_req, arcane_req]
 
-
-print(get_reqs("Rivers Of Blood",'weapons.json',roll_type['med']))
-print(get_reqs("Omen Armor",'armors.json',roll_type['light']))
-
-greatsword_req = get_reqs("Greatsword",'weapons.json',roll_type['med'])
-print(greatsword_req)
+	return req_stats
 
 
+def get_base_stats(classes_index):
+	"""Function get_base_stats
+	inputs:
+		classes_index - index of class to pull
 
+	outputs:
+		class_name - name of the class at given index
+		base_level - level of the class at given index
+		base_stats - list[vigor,mind,endurance,strength,dexterity,intelligence,faith,arcane]
+
+
+	"""
+	with open('classes.json') as f:
+		classes = json.load(f)
+
+	#INITIALIZE NAME
+	class_name		  = classes['data'][classes_index]['name']
+	#INITIALIZE BASE STATS
+	base_level		  = int(classes['data'][classes_index]['stats']['level'])
+	base_vigor		  = int(classes['data'][classes_index]['stats']['vigor'])
+	base_mind		  = int(classes['data'][classes_index]['stats']['mind'])
+	base_endurance	  = int(classes['data'][classes_index]['stats']['endurance'])
+	base_strength	  = int(classes['data'][classes_index]['stats']['strength'])
+	base_dexterity	  = int(classes['data'][classes_index]['stats']['dexterity'])
+	base_intelligence = int(classes['data'][classes_index]['stats']['intelligence'])
+	base_faith		  = int(classes['data'][classes_index]['stats']['faith'])
+	base_arcane		  = int(classes['data'][classes_index]['stats']['arcane'])
+
+
+	base_stats = [base_vigor, base_mind, base_endurance,
+	base_strength, base_dexterity, base_intelligence, base_faith, base_arcane]
+	
+	return class_name, base_level, base_stats
+
+
+def optimize_class(req_stats):
+
+
+	#LIST FORM
+	needed_stats = [0,0,0,0,0,0,0,0] #INITIALIZE NEEDED STATS
+
+	current_stats = [0,0,0,0,0,0,0,0] #INITIALIZE CURRENT STATS
+
+	#TO PICK WINNER
+	lowest_level = 999
+	best_class = ""
+	best_stats = []
+
+
+	for classes_index in range(11): # 11 classes
+		class_name, base_level, base_stats = get_base_stats(classes_index)
+
+		#print(class_name) #for testing
+
+		for stat in range((len(needed_stats))): #subtract each stat (required - base) not less than 0
+			needed_stats[stat] = max(req_stats[stat] - base_stats[stat], 0)
+
+			if needed_stats[stat] != 0: #base is lower than required save required stats
+				current_stats[stat] = req_stats[stat]
+				#print(f"\tfrom requirements: {current_stats[stat]}") #for testing
+
+			else: #base is higher than required save base stats
+				current_stats[stat] = base_stats[stat]
+				#print(f"\tfrom base: {class_name} {current_stats[stat]}") #for testing
+		
+		
+
+		current_level = sum(needed_stats) + base_level
+
+		print(f"{class_name}: {current_level}")
+		print(f"\t{current_stats}")
+
+		# store the lowest level class
+
+		if current_level < lowest_level: # new winner
+
+			best_class = class_name
+			lowest_level = current_level
+			best_stats = [current_stats]
+
+		#print(best_class, best_current_stats)
+
+			
+
+	print(f"best class is:\
+		\n\t{best_class}: {lowest_level}\
+		\n\t{best_stats}")
+
+
+
+
+item_req = get_reqs("Greatsword",'weapons.json',roll_type['med'], 1900)
+#print(item_req)
+
+
+# RETURNING MIND FOR INTELLIGENCE????
+
+optimize_class(item_req)
