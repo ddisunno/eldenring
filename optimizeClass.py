c0ea4d829dfe8ec57c04ed98f91d0d0239762606
rsd3123 <57509739+rsd3123@users.noreply.github.com>
2022-09-19 02:49:28 -0400

    yuk

--- optimizeClass.py
+++ optimizeClass.py
@@ -2,7 +2,7 @@ import pandas as pd
 import armorOptimizer as armorOptimization
 import get_reqs
 
-df_vigor = pd.read_csv(r'csv\vigor.csv')
+df_vigor = pd.read_csv(r'csv/vigor.csv')
 
 def vigorLevelToHealth(level):
     index = int(df_vigor.loc[df_vigor['Level']==level].index[0])
