6977b048d25ee6baf6c40212fe18963768c281c8
rsd3123 <57509739+rsd3123@users.noreply.github.com>
2022-09-19 10:29:03 -0400

    Better print stmt for overall optimizer

--- overallOptimizer.py
+++ overallOptimizer.py
@@ -6,20 +6,21 @@ import optimizeClass
 #Todo: function that determines what level to make vigor based on desiered health total (including talismans)
 #Combines all other optimizers into one, to output the final correct build
 #If weapon AR cannot be calc, just calculates armor
-def optimizeBuild(weaponName, weaponLevel, isTwoHanded, rollType, targetLevel, targetVitality, targetEndurance, targetMind, arsenalTalisman, erdtreeTalisman, talismans, weaponNameDmg, affinity): #WeaponName is for the base weapon, weaponNameDmg is the weapon name with affinities.
+def optimizeBuild(baseName, fullName, weaponLevel, affinity, isTwoHanded, rollType, targetLevel, targetVitality, targetEndurance, targetMind, arsenalTalisman, erdtreeTalisman, talismans): #baseName is for the base weapon, fullName is the weapon name with affinities.
     ### Calc starting class
-    startingClass = optimizeClass.calcStartingClass(weaponName, targetVitality, rollType) #Might want to make this take target endurance and mind aswell
+    startingClass = optimizeClass.calcStartingClass(baseName, targetVitality, rollType) #Might want to make this take target endurance and mind aswell
    
     ### Calc Optimal Weapon Stats
-    dmgStats = weaponOptimizer.calcStatsForWeapon(targetLevel, targetVitality, targetEndurance, targetMind, startingClass, weaponNameDmg, weaponLevel, isTwoHanded, affinity, weaponName)
+    dmgStats = weaponOptimizer.calcStatsForWeapon(targetLevel, targetVitality, targetEndurance, targetMind, startingClass, fullName, weaponLevel, isTwoHanded, affinity, baseName)
 
     ### Get Optimal Armor Sets 
-    armorSets = armorOptimization.calcOptimalArmorSets(weaponName, talismans, targetEndurance, arsenalTalisman, erdtreeTalisman, rollType) #Need to change this to include talisman weight (Change talismans.json to include)
+    armorSets = armorOptimization.calcOptimalArmorSets(baseName, talismans, targetEndurance, arsenalTalisman, erdtreeTalisman, rollType) #Need to change this to include talisman weight (Change talismans.json to include)
 
     ### Add Up & Print Results ########
     build = {'StartingClass': startingClass['name'], 'Level': targetLevel, 'Vitality':targetVitality, 'Endurance': targetEndurance, 'Mind':targetMind} 
     build = {**build,**dmgStats}
 
+    print("Weapon Name: " + fullName + " | Weapon Level: " + weaponLevel + " | Roll Type: " + rollType + " | isTwoHanded: " + str(isTwoHanded))
     print("Build Stats: " + str(build))
     print('Physical Neg Set: ' + str(armorSets[0]))
     print('Poise Set: ' + str(armorSets[1]))
@@ -46,7 +47,7 @@ erdtreeTalisman = 'None' #{'Erdtree': 1.05, 'Erdtree+1': 1.065, 'Erdtree+2': 1.0
 
 talismans = ["Great-jar's Arsenal"] #Array of up to size 4 consisting of strings of the names of the user-chosen talismans
 
-totalOptimalBuild = optimizeBuild(weaponName, weaponLevel, isTwoHanded, rollType, targetLevel, targetVitality, targetEndurance, targetMind, arsenalTalisman, erdtreeTalisman, talismans, weapon, affinity)
+totalOptimalBuild = optimizeBuild(weaponName, weapon, weaponLevel, affinity, isTwoHanded, rollType, targetLevel, targetVitality, targetEndurance, targetMind, arsenalTalisman, erdtreeTalisman, talismans)
 #######################
 
 
